a31bfd130e26a887301d615405171dea8a96b940
jdhainds <jdhainds@cps.edu>
2024-12-02 14:34:59 +0000

    webproject

---
+++ script.js
@@ -0,0 +1,26 @@
+let left = document.querySelector('.left');
+let right = document.querySelector('.right');
+let leftRight = document.querySelector('.leftRight');
+let text = document.querySelector('.p');
+0
+//Choice A
+let wingstop = document.querySelector('.wingstop');
+wingstop.style.display = "none";
+
+
+left.addEventListener('click', function(){
+    leftRight.style.display ="none";
+    left.style.display = "none";
+    right.style.display = "none";
+    wingstop.style.display = "block";
+    text.innerHTML ="to eat double click";
+});
+
+//wingstop choice 1
+wingstop.addEventListener('dbclick', function(){
+
+text.innerHTML = "man I love wingstop that was so good";
+wingstop.style.display = "none";
+finishedwingsatop.style.display = "block";
+
+});
